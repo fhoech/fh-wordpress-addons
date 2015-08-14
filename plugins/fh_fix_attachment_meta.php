5951ddd4542722d9bead049ef9e5afff2d5a5eda
Florian Höch <florian.hoech@gmx.de>
2015-08-14 17:09:51 +0200

    Let user choose attachments

--- plugins/fh_fix_attachment_meta.php
+++ plugins/fh_fix_attachment_meta.php
@@ -1,11 +1,12 @@
 <?php
 /*
 Plugin Name: FH Fix Attachment Metadata
-Plugin URI: http://hoech.net/
+Plugin URI: https://github.com/fhoech/fh-wordpress-addons/blob/master/plugins/fh_fix_attachment_meta.php
 Description: Re-generate attachment metadata if necessary.
-Version: 1.0
+Version:  $Id:$
 Author: Florian Höch
-Author URI: http://hoech.net/
+Author URI: http://hoech.net
+License: GPL3
 */
 
 add_action('admin_menu', 'wmu_admin_menu');
@@ -28,21 +29,50 @@ function fh_fix_attachment_meta() {
 			$errors = 0;
 			$upload_dir = wp_upload_dir();
 
+			if (empty($_POST['aid'])) {
+			
 			$sql = sprintf("select * from %s where post_type = 'attachment'", $wpdb->posts);
 			$attachments = $wpdb->get_results($sql);
-
+			
+			?>
+			<p>
+				<form name="updater" method="post" action="<?php echo add_query_arg(array('action' => 'run')); ?>">
+				<input type="hidden" name="force" value="<?php echo empty($_POST['force']) ? 0 : 1; ?>" />
+			<?php
 			foreach ($attachments as $attachment) {
-				$meta = get_post_meta($attachment->ID);
+				?>
+					<input class='button-primary' type='submit' name='aid' value='<?php echo $attachment->ID; ?>' />
+				<?php
+			}
+			?>
+				</form>
+			</p>
+			<?php
+			}
+			else {
+				$meta = get_post_meta($_POST['aid']);
 				if (array_key_exists('_wp_attached_file', $meta)) {
 					$file = $meta['_wp_attached_file'][0];
 					$file_path = $upload_dir['basedir'] . '/' . $file;
 					if (file_exists($file_path)) {
 						$adding_meta = false;
-						if (!array_key_exists('_wp_attachment_metadata', $meta) || !empty($_POST['force'])) {
+						if (!array_key_exists('_wp_attachment_metadata', $meta) ||
+							!array_key_exists(0, $meta['_wp_attachment_metadata']) ||
+							!array_key_exists('sizes', unserialize($meta['_wp_attachment_metadata'][0])) ||
+							!empty($_POST['force'])) {
 							echo '<div class="message"><p>Generating metadata for “' . $file . '”...</p></div>';
-							$metadata = wp_generate_attachment_metadata($attachment->ID, $file_path);
-							echo '<div class="updated"><p>...metadata successfully generated for “' . $file . '”.</p></div>';
-							$adding_meta = wp_update_attachment_metadata($attachment->ID, $metadata);
+							$metadata = wp_generate_attachment_metadata($_POST['aid'], $file_path);
+
+							if ( is_wp_error( $metadata ) )
+								echo '<div class="error"><p>Error: Metadata could not be generated for “' . $file . '”: ' . $metadata->get_error_message() . '</p></div>';
+							else if ( empty( $metadata ) )
+								echo '<div class="error"><p>Error: Metadata could not be generated for “' . $file . '” (unknown reason).</p></div>';
+							else {
+								echo '<div class="updated" onclick="var s = this.getElementsByTagName(&quot;pre&quot;)[0].style; s.display = s.display == &quot;block&quot; ? &quot;none&quot : &quot;block&quot;"><p>...metadata successfully generated for “' . $file . '”.</p><pre style="display: none">';
+								print_r($metadata);
+								echo '</pre></div>';
+							}
+							$adding_meta = wp_update_attachment_metadata($_POST['aid'], $metadata);
 						}
 						if ($adding_meta) {
 							echo '<div class="updated"><p>Updated metadata for “' . $file . '”.</p></div>';
@@ -59,11 +89,10 @@ function fh_fix_attachment_meta() {
 					}
 				}
 				else {
-					echo '<div class="error"><p>Error: Attachment ID ' . $attachment->ID . ' has no associated file.</p></div>';
+					echo '<div class="error"><p>Error: Attachment ID ' . $_POST['aid'] . ' has no associated file.</p></div>';
 					$errors ++;
 				}
 			}
-
 			echo '<div id="message"><p>' . sprintf("%d attachments were updated, %d were skipped and %d had errors.", $updated, $skipped, $errors) . '</p></div>';
 		}
 		?>
