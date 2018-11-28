663ee6425366dd36a358028f4fda7fcc6fe99d86
fhoech <florian.hoech@gmx.de>
2018-11-28 03:25:47 +0100

    Remove OPcache and Admin Bar entries as those are provided by other plugins

--- plugins/fh_query-monitor_objectcache/classes/QM_Output_ObjectCache_Stats.php
+++ plugins/fh_query-monitor_objectcache/classes/QM_Output_ObjectCache_Stats.php
@@ -8,7 +8,6 @@ class FH_QM_Output_ObjectCache_Stats extends QM_Output_Html {
 	public function __construct( QM_Collector $collector ) {
 		parent::__construct( $collector );
 		add_filter( 'qm/output/menus', array( $this, 'admin_menu' ), 101 );
-		add_filter( 'qm/output/title', array( $this, 'admin_title' ), 101 );
 		add_filter( 'qm/output/menu_class', array( $this, 'admin_class' ) );
 	}
 
@@ -29,68 +28,7 @@ class FH_QM_Output_ObjectCache_Stats extends QM_Output_Html {
 		else echo '<p><span class="qm-info">Object cache information is not available</span></p>';
 		echo '</div>';
 		echo '</div>';
-		if ( function_exists('opcache_get_configuration') &&
-			   function_exists('opcache_get_status') ) {
-			echo '<div class="qm-boxed">';
-			foreach ( array( 'Configuration' => opcache_get_configuration(),
-							 'Status' => opcache_get_status( false ) ) as $title => $entries ) {
-				echo '<div class="qm-section">';
-				echo "<h3>OPcache $title</h3>";
-				echo '<table><tbody>';
-				foreach ( $entries as $key_1 => $val_1 ) {
-					if ( is_scalar( $val_1 ) )
-						echo "<tr><th>$key_1</th><td>" . trim( var_export( $val_1, true ), "'" ) . '</td></tr>';
-					else {
-						if ( $key_1 !== 'directives' ) {
-							switch ( $key_1 ) {
-								case 'memory_usage':
-									$key_title = 'Memory Usage';
-									break;
-								case 'interned_strings_usage':
-									$key_title = 'Interned Strings Usage';
-									break;
-								case 'opcache_statistics':
-									$key_title = 'Statistics';
-									break;
-								default:
-									$key_title = ucfirst( $key_1 );
-							}
-							echo '</table></tbody>';
-							echo "<h4>$key_title</h4>";
-							echo '<table><tbody>';
-						}
-						foreach ( $val_1 as $key_2 => $val_2 ) {
-							echo "<tr><th>$key_2</th><td>" . trim( var_export( $val_2, true ), "'" ) . '</td></tr>';
-						}
-					}
-				}
-				echo '</table></tbody>';
-				echo '</div>';
-			}
-			echo '</div>';
-		}
 		echo '</div>';
-	}
-
-	/**
-	 * Adds QM Object Cache stats to admin panel
-	 *
-	 * @param array $title Array of QM admin panel titles
-	 *
-	 * @return array
-	 */
-	public function admin_title( array $title ) {
-		global $wp_object_cache;
-		if ( is_object( $wp_object_cache ) &&
-			 property_exists( $wp_object_cache, 'cache_hits' ) &&
-			 property_exists( $wp_object_cache, 'cache_misses' ) ) {
-			$total = (int) $wp_object_cache->cache_misses + (int) $wp_object_cache->cache_hits;
-			$title[] = sprintf(
-				esc_html__( 'Object Cache %s%% hit rate', 'query-monitor' ),
-				number_format( 100 / $total * (int) $wp_object_cache->cache_hits, 1 )
-			);
-		}
-		return $title;
 	}
 
 	/**
